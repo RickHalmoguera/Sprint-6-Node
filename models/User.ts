bc2844e1b012f4d7ea8ad3a9f5b3230389558364
Ricardo Halmoguera <halmoguerar@gmail.com>
2023-12-18 19:45:17 +0100

    ADD: json

---
+++ models/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+    photo:string
+    id: number
+    name: string
+    email: string
+    start_date: Date
+    description: string
+    phone: number
+    is_active: boolean
+}
