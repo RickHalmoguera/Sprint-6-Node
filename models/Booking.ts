29ed91fe163d204f4feb5265cc0198a314059e11
Ricardo Halmoguera <halmoguerar@gmail.com>
2024-01-01 11:19:30 +0100

    ADD: models ans schemas

--- models/Booking.ts
+++ models/Booking.ts
@@ -1,10 +1,28 @@
-export interface BookingInterface {
-    id: number
-    name: string
-    lastname: string
-    order_date: string
-    checkout: string
-    notes: string
-    room_type: string
-    status: string
+import mongoose, { Schema, Document } from "mongoose";
+import { RoomsInterface } from "./Room"; 
+
+export interface BookingInterface extends Document {
+    name: string;
+    lastname: string;
+    order_date: Date;
+    checkin: Date;
+    checkout: Date;
+    notes: string;
+    room: RoomsInterface;
+    status: string;
 }
+
+const bookingSchema = new Schema<BookingInterface>({
+    name: { type: String, required: true },
+    lastname: { type: String, required: true },
+    order_date: { type: Date, default: Date.now, required: true },
+    checkin: { type: Date, required: true },
+    checkout: { type: Date, required: true },
+    notes: { type: String, required: true },
+    room: { type: Schema.Types.ObjectId, ref: 'Room', required: true }, 
+    status: { type: String, required: true },
+});
+
+const BookingModel = mongoose.model<BookingInterface>('Booking', bookingSchema);
+
+export default BookingModel;
